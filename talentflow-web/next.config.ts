212052328f76bac966348ba6b815d85e198d65db
Lauriel Mesquita <laurielmesquita@me.com>
2026-06-15 04:24:37 -0300

    chore: configure next.config.ts for Cache-Control headers

--- talentflow-web/next.config.ts
+++ talentflow-web/next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate, no-cache, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
